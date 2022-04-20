5326677d788d87e870d3dc1c9fd054c3d75509d3
M4TThys123 <70703948+M4TThys123@users.noreply.github.com>
2022-04-21 00:35:10 +0200

    Ready for presentation

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,15 @@
+//Variabele voor toggel
+const toggleSection = document.querySelector("#modal")
+const moodel = document.querySelector("#modal")
+
+
+// Variabele aanmaken voor de URL en memberID
 const baseURL = 'https://tribe.api.fdnd.nl/v1'
 const endpoint = '/member'
 const url = `${baseURL}${endpoint}`
 const memberId = 3
 
+// HTML elementen selecteren die vevangend moeten weorden door de API data
 const role = document.querySelector('#type')
 const nickname = document.querySelector('#nickname')
 const avatar = document.querySelector('#avatarIMG')
@@ -10,13 +17,16 @@ const naam = document.querySelector('#naam')
 
 get()
 
+// Get fuctie die de een fetch uitvoer naar de API
+// data omzetten van JSON naar JavaScript
+// Alle HTML elementen vervangen door de juiste data en deze renderen.
 async function get (){
     const response = await fetch(url)
     const data = await response.json()
 
     role.innerHTML = data.data[memberId].type
     nickname.innerHTML = data.data[memberId].nickname
-    avatar.innerHTML = data.data[memberId].url
+    avatar.src = data.data[memberId].avatar
     naam.innerHTML = `${data.data[memberId].name} ${data.data[memberId].surname}`
 }
 
